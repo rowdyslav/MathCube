a97a7d8a8e83f6b466041317afad738a2786cd3a
NikolaZhal <ryazan62nik@yandex.ru>
2024-06-08 00:20:24 +0300

    fix change misc/sample (add float)

--- misc/sample.py
+++ misc/sample.py
@@ -5,4 +5,4 @@ from maths_question_generator.arithmetic import Arithmetic
 
 def generate(opers: list[Literal["+", "-", "*", "/"]]) -> tuple[str, int]:
     a = Arithmetic(opers=opers).base()
-    return a["question"], a["answer"]
+    return a["question"], float(a["answer"])
